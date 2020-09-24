d1405a05125521d59cc1f46f09f23f8492b894d8
ben <b.talbot@qut.edu.au>
2020-09-24 12:47:17 +1000

    Redo cards, now with a feature image

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -3,6 +3,7 @@ import {
   CardActionIcon,
   CardActionIcons,
   CardActions,
+  CardMedia,
   CardPrimaryAction,
 } from '@rmwc/card';
 import {Elevation} from '@rmwc/elevation';
@@ -13,32 +14,32 @@ import Link from 'next/link';
 import styles from '../styles/card.module.scss';
 
 export default function EntryCard({cardData}) {
-  const isDataset = cardData.type === 'dataset';
+  const section = cardData.type === 'repository' ? 'code' : cardData.type;
   return (
     <Elevation z={4} wrap>
       <Card className={styles.card}>
-        <Link href={`/${isDataset ? 'datasets' : 'code'}/${cardData.id}`}>
-          <CardPrimaryAction className={styles.primary}>
-            <div className={styles.heading}>
-              <Typography use="button" className={styles.type}>
-                {isDataset ? 'Dataset' : 'Code'}
-              </Typography>
-              <Typography
-                use="body2"
-                className={`${styles.extra} ${
-                  styles[isDataset ? 'size' : 'url']
-                }`}
-              >
-                {isDataset
-                  ? cardData.size
+        <Link href={`/${section}/${cardData.id}`}>
+          <CardPrimaryAction>
+            <CardMedia sixteenByNine className={styles['image-default']} />
+            <div className={styles.footer}>
+              {cardData.type !== 'project' && (
+                <Typography
+                  use="body2"
+                  className={`${styles.extra} ${
+                    styles[section === 'dataset' ? 'size' : 'url']
+                  }`}
+                >
+                  {section === 'dataset'
                     ? cardData.size
-                    : '(size unspecified)'
-                  : cardData.url.replace(/.*\/([^\/]*\/[^\/]*)$/, '$1')}
+                      ? cardData.size
+                      : '(size unspecified)'
+                    : cardData.url.replace(/.*\/([^\/]*\/[^\/]*)$/, '$1')}
+                </Typography>
+              )}
+              <Typography use="body1" className={styles.name}>
+                {cardData.name}
               </Typography>
             </div>
-            <Typography use="body1" className={styles.name}>
-              {cardData.name}
-            </Typography>
           </CardPrimaryAction>
         </Link>
       </Card>
