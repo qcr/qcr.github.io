264c35a1de412865bed51fa149afc4a08e2f73f6
ben <b.talbot@qut.edu.au>
2021-09-08 16:31:27 +1000

    Upgrade config to be Webpack 5 compatible

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const optimisedImages = require('next-optimized-images');
+// const optimisedImages = require('next-optimized-images');
 const path = require('path');
 const withPlugins = require('next-compose-plugins');
 
@@ -8,38 +8,42 @@ const nextConfig = {
     includePaths: ['node_modules'].map((d) => path.join(__dirname, d)),
   },
   trailingSlash: true,
-  webpack(config) {
+  webpack: (config) => {
     config.resolve.roots = [__dirname];
-    config.module.rules.push({
-      test: /^repo:/,
-      loader: ['./lib/loaders/repo.js'],
-    });
-    config.module.rules.push({
-      test: /\.md$/,
-      loader: ['./lib/loaders/markdown.js'],
-    });
-    config.module.rules.push({
-      test: /\.yaml$/,
-      loader: ['./lib/loaders/yaml.js'],
-    });
-    config.module.rules.push({
-      test: /\.gif$/,
-      loader: ['./lib/loaders/gif.js'],
-    });
-    config.module.rules.push({
-      test: /\.csv$/,
-      loader: 'csv-loader',
-    });
+    config.module.rules.push(
+        ...[
+          {
+            test: /^repo:/,
+            loader: './lib/loaders/repo.js',
+          },
+          {
+            test: /\.md$/,
+            loader: './lib/loaders/markdown.js',
+          },
+          {
+            test: /\.yaml$/,
+            loader: './lib/loaders/yaml.js',
+          },
+          {
+            test: /\.gif$/,
+            loader: './lib/loaders/gif.js',
+          },
+          {
+            test: /\.csv$/,
+            loader: 'csv-loader',
+          },
+        ]
+    );
     return config;
   },
 };
 
 module.exports = withPlugins(
     [
-      [
-        optimisedImages,
-        {handleImages: ['jpeg', 'png', 'svg', 'webp'], optimizeImages: true},
-      ],
+    // [
+    //   optimisedImages,
+    //   {handleImages: ['jpeg', 'png', 'svg', 'webp'], optimizeImages: true},
+    // ],
     ],
     nextConfig
 );
