0113b2d19595311b6073cfc969385e5925fa7ddc
ben <b.talbot@qut.edu.au>
2021-10-01 13:20:23 +1000

    Increasing spacing with focus_button icon

--- src/components/focus_button.jsx
+++ src/components/focus_button.jsx
@@ -1,13 +1,21 @@
-import {Button} from '@material-ui/core';
+import {Button, makeStyles} from '@material-ui/core';
 
 import styles from '../styles/focus_button.module.scss';
 
+const iconStyle = makeStyles({
+  endIcon: {
+    marginLeft: '12px',
+  },
+});
+
 export default function FocusButton({url, text, icon, newTab}) {
+  const csIcon = iconStyle();
   return (
     <a href={url} target={newTab && '_blank'} className={styles.link}>
       <Button
         color="primary"
         variant="contained"
+        classes={csIcon}
         className={styles.button}
         endIcon={icon}
       >
