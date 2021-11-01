7d31c8a8ce191cc590d8116a402d486d1a7a1142
ben <b.talbot@qut.edu.au>
2021-11-01 21:29:43 +1000

    Migrate GIF loader to typescript

--- lib/loaders/gif.ts
+++ lib/loaders/gif.ts
@@ -1,16 +1,23 @@
-const loaderUtils = require('loader-utils');
-const path = require('path');
-const util = require('util');
+import cp from 'child_process';
+import loaderUtils from 'loader-utils';
+import path from 'path';
+import util from 'util';
 
-const exec = util.promisify(require('child_process').exec);
+import type * as webpack from 'webpack';
+
+const exec = util.promisify(cp.exec);
 
 const PATH_ROOT = 'static/gifs';
 
-async function buildOutput(content, loaderContext, cb) {
+async function buildOutput(
+  content: string,
+  loaderContext: webpack.LoaderContext<any>,
+  cb: (err: string | null, result: string) => void
+) {
   // Figure out what we are creating
-  const params = loaderContext.resourceQuery ?
-    loaderUtils.parseQuery(loaderContext.resourceQuery) :
-    {};
+  const params = loaderContext.resourceQuery
+    ? loaderUtils.parseQuery(loaderContext.resourceQuery)
+    : {};
 
   // Construct our paths
   // TODO figure out why some of the resources are coming in with a compiler
@@ -20,10 +27,10 @@ async function buildOutput(content, loaderContext, cb) {
   const outPath = path.join(
     serverHack ? path.join('..', PATH_ROOT) : PATH_ROOT,
     loaderUtils.interpolateName(
-        loaderContext,
-        `[hash].${params.webm ? 'webm' : params.image ? 'jpg' : 'gif'}`,
-        {content: content},
-    ),
+      loaderContext,
+      `[hash].${params.webm ? 'webm' : params.image ? 'jpg' : 'gif'}`,
+      {content: content}
+    )
   );
   const publicPath = serverHack ? outPath.substring(3) : outPath;
 
@@ -32,48 +39,51 @@ async function buildOutput(content, loaderContext, cb) {
   // to what I get if I just cat the file (smaller) which makes no sense to
   // me...
   loaderContext.emitFile(
-      outPath,
-    params.webm ?
-      (
-        await exec(
+    outPath,
+    params.webm
+      ? (
+          await exec(
             `ffmpeg -i ${inPath} -c:v libvpx -crf 4 -auto-alt-ref 0 -f webm -`,
             {
               encoding: 'buffer',
               maxBuffer: 1024 * 1024 * 100,
-            },
-        )
-      ).stdout :
-      params.image ?
-      (
-        await exec(`ffmpeg -ss 0 -i ${inPath} -vframes 1 -q:v 2 -f mjpeg -`, {
-          encoding: 'buffer',
-        })
-      ).stdout :
-      (
-        await exec(`cat ${inPath}`, {
-          encoding: 'buffer',
-          maxBuffer: 1024 * 1024 * 100,
-        })
-      ).stdout,
+            }
+          )
+        ).stdout
+      : params.image
+      ? (
+          await exec(`ffmpeg -ss 0 -i ${inPath} -vframes 1 -q:v 2 -f mjpeg -`, {
+            encoding: 'buffer',
+          })
+        ).stdout
+      : (
+          await exec(`cat ${inPath}`, {
+            encoding: 'buffer',
+            maxBuffer: 1024 * 1024 * 100,
+          })
+        ).stdout
   );
 
   // Return the result of the loader
   cb(
-      null,
-      `module.exports = __webpack_public_path__ + ` +
-      `${JSON.stringify(publicPath)}`,
+    null,
+    `module.exports = __webpack_public_path__ + ` +
+      `${JSON.stringify(publicPath)}`
   );
 }
 
-module.exports = function(input) {
+export default function loader(
+  this: webpack.LoaderContext<any>,
+  input: string
+) {
   const cb = this.async();
-  const res = /[^/]*$/.exec(this.resource)[0];
+  const res = /[^/]*$/.exec(this.resource)![0];
   console.log(
-      `${this.resourceQuery ? 'Processing' : 'Skipping'} gif '${res}' ...`,
+    `${this.resourceQuery ? 'Processing' : 'Skipping'} gif '${res}' ...`
   );
   buildOutput(input, this, (err, result) => {
     if (err) return cb(err);
     if (this.resourceQuery) console.log(`Finished processing gif '${res}'.`);
     cb(null, result);
   });
-};
+}
