23d04d7e445691a7690ede4300ef62fe229c15c2
Ben Talbot <b.talbot@qut.edu.au>
2021-12-06 09:06:25 +1000

    Fix bugs in typescript version & next config

--- next.config.js
+++ next.config.js
@@ -1,35 +1,24 @@
 const fs = require('fs');
 const path = require('path');
 const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
 
-const nextConfig = {
-  images: {},
-  sassOptions: {
-    includePaths: ['node_modules'].map((d) => path.join(__dirname, d)),
-  },
+/** @type {import('next').NextConfig} */
+module.exports = withPlugins([optimizedImages], {
+  images: {disableStaticImages: true},
+  reactStrictMode: true,
   trailingSlash: true,
   webpack: (config) => {
     config.resolve.roots = [__dirname];
-    config.module.rules.find((r) => r.loader == 'next-image-loader').test =
-      /\.(png|jpg|jpeg|webp|ico|bmp|svg)$/i;
     config.module.rules.push(
       ...[
         {
-          test: /\.tsx?$/,
-          loader: 'ts-loader',
-          exclude: /node_modules/,
-        },
-        {
-          test: /^repo:/,
-          loader: './lib/loaders/repo.ts',
-        },
-        {
           test: /\.md$/,
-          loader: './lib/loaders/markdown.ts',
+          loader: './lib/loaders/markdown.js',
         },
         {
           test: /\.gif$/,
-          loader: './lib/loaders/gif.ts',
+          loader: './lib/loaders/gif.js',
         },
         {
           test: /\.csv$/,
@@ -37,9 +26,6 @@ const nextConfig = {
         },
       ]
     );
-    config.resolve.extensions.push(...['.tsx', '.ts']);
     return config;
   },
-};
-
-module.exports = nextConfig;
+});
