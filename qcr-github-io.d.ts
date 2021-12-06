52b150faf61972334810daf130349a2063b22ef7
Ben Talbot <b.talbot@qut.edu.au>
2021-12-06 10:38:52 +1000

    Update custom typescript module declarations

--- qcr-github-io.d.ts
+++ qcr-github-io.d.ts
@@ -2,3 +2,14 @@ declare module '*.csv' {
   const content: string[][];
   export default content;
 }
+
+declare module '*.md' {
+  import {GrayMatterFile} from 'gray-matter';
+  const content: GrayMatterFile<string>;
+  export default content;
+}
+
+declare module '!@svgr/webpack*' {
+  const content: React.StatelessComponent<React.SVGAttributes<SVGElement>>;
+  export default content;
+}
