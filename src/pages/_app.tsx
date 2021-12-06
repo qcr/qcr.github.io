e70ab88a97ef2585362fa738e4657a571a9ae3a8
Ben Talbot <b.talbot@qut.edu.au>
2021-12-07 09:01:11 +1000

    Use Roboto as default font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,11 +17,18 @@ import '../styles/globals.scss';
 const theme = createTheme({
   palette: {primary: {main: '#00407a'}},
   typography: {
+    fontFamily:
+      'Roboto, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif',
     h3: {
       fontWeight: 'bold',
       marginBottom: '40px',
       marginTop: '16px',
       textAlign: 'center',
+    },
+    button: {
+      textTransform: 'capitalize',
+      fontSize: '1.0rem',
+      fontWeight: 'bold',
     },
     subtitle1: {
       fontStyle: 'normal',
