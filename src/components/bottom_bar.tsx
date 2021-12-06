1eb5e390703825bf9017d4fb639277d149ec4d66
Ben Talbot <b.talbot@qut.edu.au>
2021-12-07 09:08:18 +1000

    Translate bottom_bar component to styled() API

--- src/components/bottom_bar.tsx
+++ src/components/bottom_bar.tsx
@@ -1,19 +1,36 @@
 import React from 'react';
 
-import {Typography} from '@mui/material';
+import {Typography, styled} from '@mui/material';
 
-import styles from '../styles/bottom_bar.module.scss';
+const StyledBar = styled('div')(({theme}) => ({
+  backgroundColor: theme.palette.primary.main,
+  display: 'flex',
+  height: '60px',
+  justifyContent: 'space-around',
+  marginTop: '30px',
+  width: '100%',
+}));
+
+const StyledContent = styled('div')(({theme}) => ({
+  alignItems: 'center',
+  color: theme.palette.primary.contrastText,
+  display: 'flex',
+  flexDirection: 'row',
+  height: '100%',
+  justifyContent: 'space-between',
+  padding: '12px',
+}));
+
+const StyledText = styled(Typography)({
+  fontSize: '0.85rem',
+});
 
 export default function BottomBar() {
   return (
-    <div className={styles.bar}>
-      <div className={`site-bottom-bar ${styles.content}`}>
-        <div className={styles.left}></div>
-        <div className={styles.center}></div>
-        <div className={styles.right}>
-          <Typography variant="button">CRICOS No. 00213J</Typography>
-        </div>
-      </div>
-    </div>
+    <StyledBar>
+      <StyledContent>
+        <StyledText variant="body1">CRICOS No. 00213J</StyledText>
+      </StyledContent>
+    </StyledBar>
   );
 }
