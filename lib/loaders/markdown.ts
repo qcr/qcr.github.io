64168ddcfb79b8b9f7c19a3ef8a4f860ae3a0394
Ben Talbot <b.talbot@qut.edu.au>
2022-01-05 11:23:18 +1000

    Basic markdown loader without marked paths

--- lib/loaders/markdown.ts
+++ lib/loaders/markdown.ts
@@ -1,11 +1,37 @@
+import matter from 'gray-matter';
+import mdi from 'markdown-it';
+
 import type * as webpack from 'webpack';
+
+const renderer = mdi({
+  html: true,
+})
+  .use(require('markdown-it-block-embed'), {
+    containerClassName: 'embedded-block',
+  })
+  .use(require('markdown-it-prism'))
+  .use(require('markdown-it-replace-link'), {
+    replaceLink: function (link: string) {
+      // TODO: implement
+      return link;
+    },
+  });
 
 async function asyncLoader(
   input: string,
   ctx: webpack.LoaderContext<any>,
   cb: (err: string | null, result: string) => void
 ) {
-  cb(null, `export default 'loaded'`);
+  ctx.addDependency(ctx.resourcePath);
+
+  const md = matter(input);
+  md.content = renderer.render(md.content);
+
+  Object.assign(md, md.data);
+  const md_tidy = md as {[key: string]: any};
+  ['data', 'empty', 'excerpt', 'isEmpty'].forEach((f) => delete md_tidy[f]);
+
+  cb(null, `export default ${JSON.stringify(md)}`);
   return;
 }
 
@@ -13,7 +39,6 @@ export default function loader(
   this: webpack.LoaderContext<any>,
   input: string
 ) {
-  this.addDependency(this.resourcePath);
   const cb = this.async();
   asyncLoader(input, this, (err, result) => {
     if (err) return cb(Error(err));
