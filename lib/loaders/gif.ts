7ec92c35209206c0c619a94283a782853a8d3a48
Ben Talbot <b.talbot@qut.edu.au>
2022-02-26 17:48:16 +1000

    Add automatic retries to GIF loader

--- lib/loaders/gif.ts
+++ lib/loaders/gif.ts
@@ -36,39 +36,23 @@ async function asyncLoader(
   // console.log(`Output for '${inPath}':\n\t${outPath}\n\t${outPublic}`);
 
   // Emit the file at the chosen destination
+  const buffSz = /.(webp|jpg)/.test(outPath) ? undefined : 1024 * 1024 * 50;
+  const ext = outPath.substring(outPath.lastIndexOf('.') + 1);
   ctx.emitFile(
     outPath,
-    /.webp$/.test(outPath)
-      ? (
-          await exec(`ffmpeg -y -ss 0 -i ${inPath} -vframes 1 -f webp -`, {
-            encoding: 'buffer',
-          })
-        ).stdout
-      : /.jpg$/.test(outPath)
-      ? (
-          await exec(`ffmpeg -y -ss 0 -i ${inPath} -vframes 1 -f mjpeg -`, {
-            encoding: 'buffer',
-          })
-        ).stdout
-      : /.webm$/.test(outPath)
-      ? (
-          await exec(`ffmpeg -y -i ${inPath} -c:v vp9 -crf 41 -f webm -`, {
-            encoding: 'buffer',
-            maxBuffer: 1024 * 1024 * 100,
-          })
-        ).stdout
-      : (
-          await exec(
-            `p="/tmp/$(openssl rand -hex 16).mp4"; ` +
-              `ffmpeg -y -i ${inPath} -c:v libx264 -crf 25 -pix_fmt yuv420p $p; ` +
-              `cat $p; ` +
-              `rm $p`,
-            {
-              encoding: 'buffer',
-              maxBuffer: 1024 * 1024 * 100,
-            }
-          )
-        ).stdout
+    await ffmpeg(
+      ext === 'webp'
+        ? `ffmpeg -y -ss 0 -i ${inPath} -vframes 1`
+        : ext === 'jpg'
+        ? `ffmpeg -y -ss 0 -i ${inPath} -vframes 1`
+        : ext === 'webm'
+        ? `ffmpeg -y -i ${inPath} -c:v vp9 -crf 41`
+        : ext === 'mp4'
+        ? `ffmpeg -y -i ${inPath} -c:v libx264 -crf 25 -pix_fmt yuv420p`
+        : 'false',
+      ext,
+      buffSz
+    )
   );
   console.log(`Generated ${outPublic}`);
 
@@ -77,6 +61,33 @@ async function asyncLoader(
   // 'require(fn).default'... ughhh this dribble is tedious...)
   cb(null, `module.exports = ${JSON.stringify(outPublic)}`);
   return;
+}
+
+async function ffmpeg(
+  cmd: string,
+  outfileType: string,
+  bufferSize = 1024 * 1024
+) {
+  const MAX_ATTEMPTS = 3;
+  let x = 0;
+  let out = Buffer.alloc(0);
+  while (x < MAX_ATTEMPTS) {
+    try {
+      out = (
+        await exec(
+          `p=/tmp/$(openssl rand -hex 64).${outfileType}; ` +
+            `${cmd} $p; cat $p; rm $p`,
+          {encoding: 'buffer', maxBuffer: bufferSize}
+        )
+      ).stdout;
+      x = MAX_ATTEMPTS;
+    } catch (e) {
+      x += 1;
+      if (x == MAX_ATTEMPTS) throw e;
+      await new Promise((r) => setTimeout(r, 5000));
+    }
+  }
+  return out;
 }
 
 export default function loader(
