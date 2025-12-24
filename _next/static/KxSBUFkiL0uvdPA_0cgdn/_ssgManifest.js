b489ca94353560dd59b90ea9661f792473ce1e7a
GitHub Action <actions@github.com>
2025-12-24 00:01:35 +0000

    Generated from contents of master commit: d59d600

---
+++ _next/static/KxSBUFkiL0uvdPA_0cgdn/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002F[list]","\u002Fcode\u002F[code]","\u002Fcollection\u002F[collection]","\u002Fdataset\u002F[dataset]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
